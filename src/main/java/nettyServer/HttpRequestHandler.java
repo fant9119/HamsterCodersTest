0d82f2193550158cedad30e4f0d6661740134530
Kutsenko Roman <rk@dzi.kiev.ua>
2015-12-16 16:34:15 +0200

    adding 404.html page and edit some changes to HttpRequestHandler'

--- src/main/java/nettyServer/HttpRequestHandler.java
+++ src/main/java/nettyServer/HttpRequestHandler.java
@@ -1,17 +1,28 @@
 package nettyServer;
 
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.DefaultFileRegion;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.DefaultHttpResponse;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.FullHttpResponse;
 import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpResponse;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.codec.http.LastHttpContent;
 import io.netty.handler.codec.http.QueryStringDecoder;
 import io.netty.util.CharsetUtil;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.concurrent.TimeUnit;
 import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandler.Sharable;
 
@@ -38,7 +49,7 @@ public class HttpRequestHandler extends SimpleChannelInboundHandler<FullHttpRequ
 				statusResponse(ctx);
 				break;
 			default:
-				pageNotFoundResponse(ctx);
+				pageNotFoundResponse(ctx, msg);
 				break;
 		}
 	}
@@ -48,12 +59,36 @@ public class HttpRequestHandler extends SimpleChannelInboundHandler<FullHttpRequ
 		
 	}
 
-	private void pageNotFoundResponse(ChannelHandlerContext ctx) {
+	private void pageNotFoundResponse(ChannelHandlerContext ctx, FullHttpRequest request) throws Exception {
+	/*	File file = new File("404.html");
+	    char[] chars = new char[(int)file.length()];
+		try {
+	    	FileReader fr = new FileReader(file);
+			fr.read(chars);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.NOT_FOUND,  
-				Unpooled.copiedBuffer("404 Sorry, but this page not found.", CharsetUtil.UTF_8));
+				Unpooled.copiedBuffer(chars, CharsetUtil.UTF_8));
         response.headers().set(HttpHeaders.Names.CONTENT_TYPE, "text/html");
 		ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
-		System.out.println(response);
+		System.out.println(response);*/
+		RandomAccessFile file = new RandomAccessFile("404.html", "r");
+	
+		HttpResponse response = new DefaultHttpResponse(request.getProtocolVersion(), HttpResponseStatus.NOT_FOUND);
+        response.headers().set(HttpHeaders.Names.CONTENT_TYPE, "text/html; charset=UTF-8");
+        boolean keepAlive = HttpHeaders.isKeepAlive(request);
+        if(keepAlive) {
+        	response.headers().set(HttpHeaders.Names.CONTENT_LENGTH, file.length());
+        	response.headers().set(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
+        }
+        ctx.write(response);
+        ctx.write(new DefaultFileRegion(file.getChannel(), 0, file.length()));
+        ChannelFuture future = ctx.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT);
+        if(!keepAlive) {
+        	future.addListener(ChannelFutureListener.CLOSE);
+        }
 	}
 
 	private void helloResponse(ChannelHandlerContext ctx) {
